2908a66eab33eb2fc31398987ac29611947190aa
mcclaren00 <mcclaren00@gmail.com>
2022-05-22 15:33:08 -0400

    adding terraform

---
+++ provision/terraform/proxmox/srv-ubnt-1.tf
@@ -0,0 +1,17 @@
+resource "proxmox_vm_qemu" "srv_ubnt_1" {
+    name = "srv-ubnt-1"
+    desc = "Ubuntu Server"
+    vmid = "401"
+    target_node = "proxmox"
+
+    agent = 1
+    onboot = 1
+    clone = "ubnt-svr-temp"
+
+    cores = 1
+    sockets = 1
+    cpu = "host"
+    memory = 1024
+
+    os_type = "cloud-init"
+}
