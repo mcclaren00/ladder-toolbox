04f7518470c45de55f5eb98a5ba9696430a518a0
mcclaren00 <mcclaren00@gmail.com>
2022-05-22 15:34:38 -0400

    tf provider

--- provision/terraform/proxmox/provider.tf
+++ provision/terraform/proxmox/provider.tf
@@ -2,7 +2,7 @@ terraform {
     required_version = ">= 0.13.0"
     required_providers {
         proxmox = {
-            source = "telemate/proxmox"
+            source = "telmate/proxmox"
             version = "2.9.3"
 
         }
