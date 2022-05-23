47231fd61cd0a47abd9b2dc5788c2c893a441f3b
yogshur <jakeit555@gmail.com>
2022-05-22 22:29:26 -0400

    Updated Index

--- upsite/src/index.js
+++ upsite/src/index.js
@@ -1,9 +1,4 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <h1>React App</h1>
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+ReactDOM.render(<site />, document.getElementById("root"))
