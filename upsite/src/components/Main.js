8a4146286fd6c68563c2a751daf362085fa5e51f
yogshur <jakeit555@gmail.com>
2022-05-23 00:45:07 -0400

    Updated Mainbar

---
+++ upsite/src/components/Main.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+export default function Main()  {
+    return  (
+        <Main>
+            <h2>Reliable Secure Storage on A Decentralized Network</h2>
+            <button>Free Trial</button>
+            <button>Sign up Now</button>
+            <button>More info</button>
+            <h4>Provided Services</h4>
+        </Main>
+    )
+}
--- upsite/src/site.js
+++ upsite/src/site.js
@@ -1,10 +1,12 @@
 import React from "react"
 import Navbar from "./components/Navbar"
+import Main from "./components/Main"
 
 export default function Site()   {
     return  (
         <div className = "container">
             <Navbar />
+            <Main   />
         </div>
     )
 }
