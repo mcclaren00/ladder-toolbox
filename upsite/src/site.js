56e72555e158dec3a62bc71739922f4d851f52df
yogshur <jakeit555@gmail.com>
2022-05-22 23:03:50 -0400

    new functions

---
+++ upsite/src/site.js
@@ -0,0 +1,11 @@
+import React from "react"
+import Navbar from "./componenets/Navbar"
+import Main from "./componenets/Main"
+
+export default function App()   {
+    return  (
+        <div className = "container">
+            
+        </div>
+    )
+}
