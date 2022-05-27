9a6d3054b5355810c08dc681b9fc912446efca69
mcclaren00 <mcclaren00@gmail.com>
2022-05-27 14:04:40 -0400

    terraform

--- provision/terraform/cloudflare/main.tf
+++ provision/terraform/cloudflare/main.tf
@@ -1,43 +1,39 @@
 terraform {
 
-    required_providers {
-        cloudflare = {
-            source = "cloudflare/cloudflare"
-            version = "3.14.0"
-        }
-        http = {
-            source = "hashicorp/http"
-            version: "2.1.0"
-        }
-        sops = {
-            source = "carlpett/sops"
-            version: "0.7.0"
-        }
+  required_providers {
+    cloudflare = {
+      source  = "cloudflare/cloudflare"
+      version = "3.15.0"
     }
+    http = {
+      source  = "hashicorp/http"
+      version = "2.1.0"
+    }
+    sops = {
+      source  = "carlpett/sops"
+      version = "0.7.1"
+    }
+  }
 }
 
 data "sops_file" "cloudflare_secrets" {
-    source_file = "secret.sops.yaml"
+  source_file = "secret.sops.yaml"
 }
 
 provider "cloudflare" {
-    email = data.sops_file.cloudflare_secrets.data["cloudflare_email"]
-    api_key = data.sops_file.cloudflare_secrets.data["cloudflare_apikey"]
-  
+  email   = data.sops_file.cloudflare_secrets.data["cloudflare_email"]
+  api_key = data.sops_file.cloudflare_secrets.data["cloudflare_apikey"]
 }
 
 data "cloudflare_zones" "domain" {
-    filter {
-      name = data.sops_file.cloudflare_secrets.data["cloudflare_domain"]
-    }
+  filter {
+    name = data.sops_file.cloudflare_secrets.data["cloudflare_domain"]
+  }
 }
 
 resource "cloudflare_zone_settings_override" "cloudflare_settings" {
-    zone_id = lookup(data.cloudflare_zones.domain.zones[0], "id")
+  zone_id = lookup(data.cloudflare_zones.domain.zones[0], "id")
   settings {
-    # /ssl tls 
-
-
     ssl = "strict"
     always_use_https         = "on"
     min_tls_version          = "1.2"
@@ -45,14 +41,10 @@ resource "cloudflare_zone_settings_override" "cloudflare_settings" {
     tls_1_3                  = "zrt"
     automatic_https_rewrites = "on"
     universal_ssl            = "on"
-
-    # /firewall/settings
     browser_check  = "on"
     challenge_ttl  = 1800
     privacy_pass   = "on"
     security_level = "medium"
-
-    # /optimization
     brotli = "on"
     minify {
       css  = "on"
@@ -60,11 +52,8 @@ resource "cloudflare_zone_settings_override" "cloudflare_settings" {
       html = "on"
     }
     rocket_loader = "on"
-    # cachihg configurations
     always_online    = "off"
     development_mode = "off"
-
-    # /network stuff
     http3               = "on"
     zero_rtt            = "on"
     ipv6                = "on"
@@ -72,13 +61,9 @@ resource "cloudflare_zone_settings_override" "cloudflare_settings" {
     opportunistic_onion = "on"
     pseudo_ipv4         = "off"
     ip_geolocation      = "on"
-
-    # /content protection
     email_obfuscation   = "on"
     server_side_exclude = "on"
     hotlink_protection  = "off"
-
-    # /workers
     security_header {
       enabled = false
     }
@@ -106,4 +91,3 @@ resource "cloudflare_record" "root" {
   type    = "CNAME"
   ttl     = 1
 }
-  
