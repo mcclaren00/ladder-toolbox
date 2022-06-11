0c9894c0c92efba8428940e758a8040684204ed9
mcclaren00 <mcclaren00@gmail.com>
2022-06-11 17:15:20 -0400

    cloudflare

--- cluster/apps/networking/cloudflare-ddns/cloudflare-ddns.sh
+++ cluster/apps/networking/cloudflare-ddns/cloudflare-ddns.sh
@@ -5,14 +5,14 @@ set -o errexit
 
 current_ipv4="$(curl -s https://ipv4.icanhazip.com/)"
 zone_id=$(curl -s -X GET \
-    "https://api.cloudflare.com/client/v4/zones?name=${CLOUDFLARE_RECORD_NAME#*.}&status=active" \
+    "https://api.cloudflare.com/client/v4/zones?name=${BOOTSTRAP_CLOUDFLARE_DOMAIN#*.}&status=active" \
     -H "X-Auth-Email: ${CLOUDFLARE_EMAIL}" \
     -H "X-Auth-Key: ${CLOUDFLARE_APIKEY}" \
     -H "Content-Type: application/json" \
         | jq --raw-output ".result[0] | .id"
 )
 record_ipv4=$(curl -s -X GET \
-    "https://api.cloudflare.com/client/v4/zones/${zone_id}/dns_records?name=${CLOUDFLARE_RECORD_NAME}&type=A" \
+    "https://api.cloudflare.com/client/v4/zones/${zone_id}/dns_records?name=${BOOTSTRAP_CLOUDFLARE_DOMAIN}&type=A" \
     -H "X-Auth-Email: ${CLOUDFLARE_EMAIL}" \
     -H "X-Auth-Key: ${CLOUDFLARE_APIKEY}" \
     -H "Content-Type: application/json" \
@@ -28,7 +28,7 @@ update_ipv4=$(curl -s -X PUT \
     -H "X-Auth-Email: ${CLOUDFLARE_EMAIL}" \
     -H "X-Auth-Key: ${CLOUDFLARE_APIKEY}" \
     -H "Content-Type: application/json" \
-    --data "{\"id\":\"${zone_id}\",\"type\":\"A\",\"proxied\":true,\"name\":\"${CLOUDFLARE_RECORD_NAME}\",\"content\":\"${current_ipv4}\"}" \
+    --data "{\"id\":\"${zone_id}\",\"type\":\"A\",\"proxied\":true,\"name\":\"${BOOTSTRAP_CLOUDFLARE_DOMAIN}\",\"content\":\"${current_ipv4}\"}" \
 )
 if [[ "$(echo "$update_ipv4" | jq --raw-output '.success')" == "true" ]]; then
     printf "%s - Success - IP Address '%s' has been updated" "$(date -u)" "${current_ipv4}"
