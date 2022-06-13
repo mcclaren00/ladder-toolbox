8c6f71fbb1a831c6f9eb268c2b57f0a125d3eddf
Copeland Peacock <peacockcopeland@gmail.com>
2022-06-13 19:04:35 -0400

    Hello World

--- upsite/src/index.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import Site from "./site"
-const container = document.getElementById('root');
-const root = createRoot(container);
-
-root.render(<Site />);
