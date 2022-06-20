7afca7812d9115a63762e06b17baf255d87d4eb3
PeacockCopeland <peacockcopeland@gmail.com>
2022-06-20 14:39:43 -0400

    All info pages made into one infopage

--- upsite/src/site.js
+++ upsite/src/site.js
@@ -1,45 +1,38 @@
 import React, { Fragment} from "react";
 import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
-import FreeTrial from "./components/FreeTrial";
-import Signup from "./components/Signup";
-import Moreinfo from "./components/Moreinfo";
-import Home from "./components/Home";
-import PrivateRoute from "./components/PrivateRoute";
-import Redundancy from "./components/Redundancy";
-import Security from "./components/Security";
-import Decentralize from "./components/Decentralization";
-import CloudProviders from "./components/CloudProviders";
-import Upload from "./components/Upload";
-import Main from "./webPages/Main.js";
 
+import Signup from "./components/Signup";
+import PrivateRoute from "./components/PrivateRoute";
+
+
+import Home from "./webPages/Home.js"
+import Upload from "./webPages/Upload.js"
+import InfoPage from "./webPages/InfoPage.js"
 
 export default function site ()  {
-
     return  (
         <Router>
             <Fragment>
                 <Routes>
-                    <Route excat path="/Main" element={<Main/>}>
-                    </Route>
                     <Route exact path="/" element={<PrivateRoute/>}>
                     </Route>
                     <Route exact path="/Home" element={<Home/>}>
                     </Route>
                     <Route exact path="/Upload" element={<Upload/>}>
                     </Route>
-                    <Route exact path="/FreeTrial" element={<FreeTrial/>}>
+                    <Route exact path="/FreeTrial" element={<InfoPage ptype="FreeTrial"/>}>
                     </Route>
                     <Route exact path="/Signup" element={<Signup/>}>
                     </Route>
-                    <Route exact path="/Moreinfo" element={<Moreinfo/>}>
+                    <Route exact path="/Moreinfo" element={<InfoPage ptype="MoreInfo"/>}>
                     </Route>
-                    <Route exact path="/Redundancy" element={<Redundancy/>}>
+                    <Route exact path="/Redundancy" element={<InfoPage ptype="Redundancy"/>}>
                     </Route>
-                    <Route exact path="/Security" element={<Security/>}>
+                    <Route exact path="/Security" element={<InfoPage ptype="Securityh"/>}>
                     </Route>
-                    <Route exact path="/Decentralize" element={<Decentralize/>}>
+                    <Route exact path="/Decentralize" element={<InfoPage ptype="Decentralize"/>}>
                     </Route>
-                    <Route exact path="/CloudProviders" element={<CloudProviders/>}>
+                    <Route exact path="/CloudProviders" element={<InfoPage ptype="CloudProviders"/>}>
                     </Route>
                 </Routes>
             </Fragment>
