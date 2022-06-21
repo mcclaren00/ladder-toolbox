04fbe82a7adc07e05c0460bc3d2f2ba790005da5
PeacockCopeland <peacockcopeland@gmail.com>
2022-06-21 15:57:39 -0400

    footer css inprogress

--- upsite/src/components/Footer.js
+++ upsite/src/components/Footer.js
@@ -2,28 +2,34 @@ import React from "react"
 
 export default function Footer()    {
     return  (
-        <footer>
-            <h5 className="usJoin">Join us</h5>
-            <ul className="logUp">
-                <li>Discord</li>
-                <li>Twitter</li>
-                <li>Github</li>
-                <li>Signup</li>
-                <li>Login</li>
-            </ul>
-            <h5 className="standOur">Our standards</h5>
-            <ul className="policyUse">
-                <li>Privacy</li>
-                <li>Security</li>
-                <li>Terms of service</li>
-                <li>Acceptable use policy</li>
-            </ul>
-            <h5 className="usHelp">Help</h5>
-            <ul className="recoveryBack">
-                <li>Support</li>
-                <li>More information</li>
-                <li>Backup/Recovery</li>
-            </ul>
+        <footer className="footer--Container">
+            <div>
+                <h5 className="footer--Label">Join us</h5>
+                <ul className="footer--JoinList">
+                    <li>Discord</li>
+                    <li>Twitter</li>
+                    <li>Github</li>
+                    <li>Signup</li>
+                    <li>Login</li>
+                </ul>
+            </div>
+            <div>
+                <h5 className="footer--Label">Our standards</h5>
+                <ul className="footer--StandardsList">
+                    <li>Privacy</li>
+                    <li>Security</li>
+                    <li>Terms of service</li>
+                    <li>Acceptable use policy</li>
+                </ul>
+            </div>
+            <div>
+                <h5 className="footer--Label">Help</h5>
+                <ul className="footer--HelpList">
+                    <li>Support</li>
+                    <li>More information</li>
+                    <li>Backup/Recovery</li>
+                </ul>
+            </div>
         </footer>
     )
 }
