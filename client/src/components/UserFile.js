d45eccca53aeeb572087abf8a074d075ce5d71d8
PeacockCopeland <peacockcopeland@gmail.com>
2022-07-14 18:39:08 -0400

    css

--- client/src/components/UserFile.js
+++ client/src/components/UserFile.js
@@ -3,9 +3,9 @@ import React from "react"
 const UserFile = props => {
     return(
         <div>
-            <p>{props.item.title}</p>
-            <p>{props.item.kind}</p>
-            <p>{props.item.dateLO}</p>
+            <p className="UserFile--Pieces">{props.item.title}</p>
+            <p className="UserFile--Pieces">{props.item.kind}</p>
+            <p className="UserFile--Pieces">{props.item.dateLO}</p>
         </div>
     )
 }
