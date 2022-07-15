62975ef31bd44477a1d8bf55fc0c8f0cded6b215
PeacockCopeland <peacockcopeland@gmail.com>
2022-07-15 17:34:54 -0400

    css to home page work in progress

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -8,8 +8,8 @@ const Navbar = () => {
 
     return  (
         <nav className="navBar--Container">
-            <img src={Logo} className="navBar--LogoImg" alt=""/>
             <button className="navBar--LadderButton" onClick={() => {navigate("/Home")}}>LadderCorp</button>
+            <img src={Logo} className="navBar--LogoImg" alt=""/>
             <button className="navBar--LoginButton" onClick ={() => {navigate("/Login")}}>Login</button>
         </nav>
     )
--- client/src/webPages/Home.js
+++ client/src/webPages/Home.js
@@ -3,6 +3,7 @@ import frontPageData from "../data/frontPageData";
 import ButtonCreation from "../components/ButtonCreation";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
+import Logo from "../images/ladder_corp.png"
 
 const Home = () => {
     const buttons = frontPageData.map(item => {
@@ -19,7 +20,8 @@ const Home = () => {
             <div className="mainGrid--Wrapper">
                 <Navbar />
                 <main className="home--Container">
-                    <h2>Reliable Secure Storage on A Decentralized Network</h2>
+                    <span className="home--TitleText">Reliable Secure Storage on A <span className="home--TitleTextBlue">Decentralized</span> Network</span>
+                    <img src={Logo} className="home--LogoImg" alt=""/>
                     <div className="home--ButtonContainer">
                         {buttons}
                     </div>
